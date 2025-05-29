319dcc4b3cc81b6abe6c2fbac1fc95697510c199
unknown <gustazelante@gmail.com>
2025-05-29 00:35:16 -0300

    Nada demais

---
+++ src/main/java/pacote/Tarefa.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pacote;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin
+ */
+public class Tarefa {
+   private int contador = 0;
+   private int id;
+   private String titulo;
+   private String desc;
+   private String prioridade;
+   private String status;    
+   
+/*Getters e Setters*/
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+/*Construtores*/
+    public Tarefa(String desc) {
+        this.desc = desc;
+    }
+    
+/*Metodos*/
+    
+    public void Create(){
+        
+    }
+}
--- src/main/java/pacote/TaskMaster.java
+++ src/main/java/pacote/TaskMaster.java
@@ -2,14 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package teste;
+package pacote;
+
+import java.util.List;
 
 /**
  *
  * @author admin
  */
-public class Testte {
-    public static void main(String[] args) {
-        System.out.print("Ola mundo, Arquivo Atualizado");
-    }
+public class TaskMaster {    
 }
